e3bd0428135e945950c791d29898b90aa7e12ed0
Degra08 <adegracia2@dxc.com>
2024-03-05 19:31:49 +0100

    Commit 5

--- src/main/java/com/banana/proyectostareas/controller/ProyectoServiceController.java
+++ src/main/java/com/banana/proyectostareas/controller/ProyectoServiceController.java
@@ -2,6 +2,7 @@ package com.banana.proyectostareas.controller;
 
 import com.banana.proyectostareas.model.Proyecto;
 import com.banana.proyectostareas.persistence.ProyectoJPARepository;
+import com.banana.proyectostareas.model.StatusMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,8 +29,11 @@ public class ProyectoServiceController {
 
     @PostMapping(value = "", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<Proyecto> save(@RequestBody @Valid Proyecto newProyecto) {
-        logger.info("newProyecto:" + newProyecto);
         newProyecto.setId(null);
-        return new ResponseEntity<>(repo.save(newProyecto), HttpStatus.CREATED);
+        repo.save(newProyecto);
+        if (newProyecto != null && newProyecto.getId() > 0) return new ResponseEntity<>(newProyecto, HttpStatus.CREATED);
+        else
+            return new ResponseEntity<>(new StatusMessage(HttpStatus.BAD_REQUEST.value(), "No encontrado"), HttpStatus.BAD_REQUEST);
+
     }
 }
