4bd85dacb79f6eaaa9ae74b14da0e41c360e73bf
Degra08 <adegracia2@dxc.com>
2024-03-05 18:47:00 +0100

    Commit 2

---
+++ src/main/java/com/banana/proyectostareas/controller/ProyectoServiceController.java
@@ -0,0 +1,35 @@
+package com.banana.proyectostareas.controller;
+
+import com.banana.proyectostareas.model.Proyecto;
+import com.banana.proyectostareas.persistence.ProyectoJPARepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/clients")
+@Validated
+public class ProyectoServiceController {
+
+    private static final Logger logger = LoggerFactory.getLogger(ProyectoServiceController.class);
+
+    @Autowired
+    private ProyectoJPARepository repo;
+
+    @PostMapping(value = "", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public ResponseEntity<Proyecto> save(@RequestBody @Valid Proyecto newProyecto) {
+        logger.info("newProyecto:" + newProyecto);
+        newProyecto.setId(null);
+        return new ResponseEntity<>(repo.save(newProyecto), HttpStatus.CREATED);
+    }
+}
--- src/main/java/com/microcompany/taskmanager/TaskmanagerApplication.java
+++ src/main/java/com/microcompany/taskmanager/TaskmanagerApplication.java
@@ -1,10 +1,14 @@
 package com.microcompany.taskmanager;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TaskmanagerApplication {
+	private static final Logger logger = LoggerFactory.getLogger(TaskmanagerApplication.class);
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskmanagerApplication.class, args);
