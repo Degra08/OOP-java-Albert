f9de7d2d45e2223de65d24ba6b20fcbfd5437267
Degra08 <adegracia2@dxc.com>
2024-03-05 19:19:12 +0100

    Commit 4

--- src/main/java/com/banana/proyectostareas/model/Proyecto.java
+++ src/main/java/com/banana/proyectostareas/model/Proyecto.java
@@ -1,16 +1,35 @@
 package com.banana.proyectostareas.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
+
+import javax.persistence.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
-@Data
+@Entity
+@Table(name = "products")
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 public class Proyecto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Min(0)
     private Long id;
+
+    @Column
+    @NotBlank
+    @NotNull
+    @Size(min = 3, max = 50)
     private String nombre;
+
+    @DateTimeFormat
     private LocalDate fechaCreacion;
 }
