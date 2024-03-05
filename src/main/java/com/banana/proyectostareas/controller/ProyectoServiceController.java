3f78c793707fd229c0a3f7e78fa75cc05d6dcb3a
Degra08 <adegracia2@dxc.com>
2024-03-05 18:47:38 +0100

    Commit 3

--- src/main/java/com/banana/proyectostareas/controller/ProyectoServiceController.java
+++ src/main/java/com/banana/proyectostareas/controller/ProyectoServiceController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/clients")
+@RequestMapping("/proyectos")
 @Validated
 public class ProyectoServiceController {
 
